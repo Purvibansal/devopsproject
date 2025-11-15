0fdbf5b27e6f6bce07945a64ae9127dc9721a6e3
PurviBansal <purvibansal87@gmail.com>
2025-11-15 12:56:03 +0530

    correct program

--- Pro.java
+++ Pro.java
@@ -3,7 +3,7 @@ public class Pro {
     public static void main(String[] args) {
         System.out.println("Running Basic Jenkins CI Java Test");
 
-        Calculator calc = new Calculator();
+        cal calc = new cal();
 
         int mul = calc.multiply(5, 4);
         int sub = calc.subtract(10, 3);
